65e91557926d03944b246d01310b60b1c9b39345
EvandroVogel <65269211+EvandroVogel@users.noreply.github.com>
2020-07-20 23:36:27 -0300

    Add files via upload

---
+++ Primos.java
@@ -0,0 +1,50 @@
+
+public class Primos {
+	public static void main(String[] args) {
+		int contador = 0;
+		int primo = 0;
+		while (primo <= (6 * 5 * 7)) {
+			++contador;
+			primo = 30 * contador + 11;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 13;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 17;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 19;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 23;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 29;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 31;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 30 * contador + 37;
+			if (primo % 7 != 0) {
+				System.out.println(primo);
+			}
+
+		}
+	}
+}
---
+++ Primos2.java
@@ -0,0 +1,239 @@
+
+public class Primos2 {
+	public static void main(String[] args) {
+		int contador = 0;
+		int primo = 0;
+		while (primo <= (6 * 5 * 7 * 11)) {
+			++contador;
+
+			primo = 210 * contador + 13;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 210 * contador + 17;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 210 * contador + 19;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 210 * contador + 23;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 210 * contador + 29;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 210 * contador + 31;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+			primo = 210 * contador + 37;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 41;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 43;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 47;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 53;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 59;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 61;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 67;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 71;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 73;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 79;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 83;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 89;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 97;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 101;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 103;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 107;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 109;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 113;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 121;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			primo = 210 * contador + 127;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 131;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 139;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 143;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 149;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 151;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			primo = 210 * contador + 157;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 163;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 167;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 169;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 173;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 179;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 181;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 187;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 191;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 193;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 197;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 199;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 209;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+			
+			primo = 210 * contador + 211;
+			if (primo % 11 != 0) {
+				System.out.println(primo);
+			}
+
+		}
+	}
+}

2f4811a6146160d582dbb7b929e6d8063a05143a
EvandroVogel <65269211+EvandroVogel@users.noreply.github.com>
2020-07-21 00:26:14 -0300

    Add files via upload

--- Primos.java
+++ Primos.java
@@ -3,48 +3,17 @@ public class Primos {
 	public static void main(String[] args) {
 		int contador = 0;
 		int primo = 0;
-		while (primo <= (6 * 5 * 7)) {
+		while (primo <= (6 * 5 )) {
 			++contador;
-			primo = 30 * contador + 11;
-			if (primo % 7 != 0) {
+			primo = 6 * contador + 5;
+			if (primo % 5 != 0) {
 				System.out.println(primo);
 			}
 
-			primo = 30 * contador + 13;
-			if (primo % 7 != 0) {
+			primo = 6 * contador + 7;
+			if (primo % 5 != 0) {
 				System.out.println(primo);
 			}
-
-			primo = 30 * contador + 17;
-			if (primo % 7 != 0) {
-				System.out.println(primo);
-			}
-
-			primo = 30 * contador + 19;
-			if (primo % 7 != 0) {
-				System.out.println(primo);
-			}
-
-			primo = 30 * contador + 23;
-			if (primo % 7 != 0) {
-				System.out.println(primo);
-			}
-
-			primo = 30 * contador + 29;
-			if (primo % 7 != 0) {
-				System.out.println(primo);
-			}
-
-			primo = 30 * contador + 31;
-			if (primo % 7 != 0) {
-				System.out.println(primo);
-			}
-
-			primo = 30 * contador + 37;
-			if (primo % 7 != 0) {
-				System.out.println(primo);
-			}
-
 		}
 	}
 }
